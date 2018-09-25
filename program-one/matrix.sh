19cd82857bd80d0cbcd4cdf2d4a6309f5127e8b5
Logan Saso <logan.saso2016@gmail.com>
2018-09-24 17:42:43 -0700

    Gets correct lines regardless of source

--- program-one/matrix.sh
+++ program-one/matrix.sh
@@ -1,56 +1,21 @@
 #!/bin/bash
 
+# $1 == operation
+# $2 == m1file if any
+# $3 == m2file if any
 
-MATRIXONE=()
-MATRIXTWO=()
-read_file () {
-    index=0
-    while IFS=$'' read -r line; do
-        # echo "Reading Line"
-        local numbers=()
+if [ "$1" = "dims" ]
+then
 
-        # echo "Text read from file: ${line}"
+    lines=0
 
-        echo $line | while IFS=$' ' read -r -a numbers; do
-            # echo "Reading into Array"
-            innerindex=0
-            for num in "${numbers[@]}"
-            do
-                echo "Num: $num"
-                echo "Inner Index: $innerindex"
-                innerindex=`expr $innerindex + 1`
-                MATRIXONE["$index","$innerindex"]="$num"
-                # echo "Index: $index"
-                echo "M1: ${MATRIXONE[$index,$innerindex]}"
-
-            done
-        done
-        index=`expr $index + 1`
-
-    done < "$1"
-}
-
-print_matrix_one () {
-
-    rows=${#MATRIXONE[@]}
-    echo "Rows: $rows"
-    rowcount=0
-    until [ $rowcount -ge $rows ];
+    while read line
     do
-        cols=${#MATRIXONE[$rowcount,@]}
-        colcount=0
-        until [ $colcount -ge $cols ];
-        do
+        echo "Line: $line"
+        lines=`expr $lines + 1`
+    done < ${2:-/dev/stdin}
 
-            colcount=`expr $colcount + 1`
-        done
 
-        rowcount=`expr $rowcount + 1`
 
-    done
-
-}
-
-read_file $1 $2
-
-print_matrix_one
+    echo "$lines $cols"
+fi
