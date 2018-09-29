42a819df19dd3f8da3c2f732cbb9de1ce4e593c9
Logan Saso <sasol@oregonstate.edu>
2018-09-29 02:38:11 -0700

    Added more backbone (plus add and mult functions)

--- program-one/matrix.sh
+++ program-one/matrix.sh
@@ -4,18 +4,87 @@
 # $2 == m1file if any
 # $3 == m2file if any
 
-if [ "$1" = "dims" ]
-then
-
+# $1 is the matrix to get a dim
+dims () {
     lines=0
 
     while read line
     do
-        echo "Line: $line"
-        lines=`expr $lines + 1`
-    done < ${2:-/dev/stdin}
+        latestline=$line
+        lines=$(( $lines + 1 ))
+    done < $1
+    cols=`awk '{print NF}' <<< $latestline | sort -nu | tail -n 1`
 
-    cols=`awk '{print NF}' $line | sort -nu | tail -n 1`
+    echo -e "$lines $cols"
+}
 
-    echo "$lines $cols"
+# $1 is the first matrix
+# $2 is the second matrix
+add () {
+    thedims=$(dims $1)
+    cutrows=`echo $thedims | cut -c 1`
+    cutcols=`echo $thedims | cut -c 3`
+
+    for ((i=1;i<$cutcols;i++))
+    do
+        lineone=`head -n $i < $1 | tail -n 1`
+        linetwo=`head -n $i < $2 | tail -n 1`
+        # echo "Lineone: $lineone"
+        # echo "Linetwo: $linetwo"
+        for ((x=1;x<$cutcols*2;x+=2))
+        do
+
+            colone=`echo $lineone | cut -c $x`
+            coltwo=`echo $linetwo | cut -c $x`
+            echo -n -e "$(( $colone + $coltwo ))\t"
+        done
+        echo ""
+    done
+}
+
+mult () {
+    thedims=$(dims $1)
+    cutrows=`echo $thedims | cut -c 1`
+    cutcols=`echo $thedims | cut -c 3`
+
+    for ((i=1;i<$cutcols;i++))
+    do
+        lineone=`head -n $i < $1 | tail -n 1`
+        linetwo=`head -n $i < $2 | tail -n 1`
+        # echo "Lineone: $lineone"
+        # echo "Linetwo: $linetwo"
+        for ((x=1;x<$cutcols*2;x+=2))
+        do
+
+            colone=`echo $lineone | cut -c $x`
+            coltwo=`echo $linetwo | cut -c $x`
+            echo -n -e "$(( $colone * $coltwo ))\t"
+        done
+        echo ""
+    done
+}
+
+if [[ "$1" = "dims" && ( ("$#" -eq 1 && -n ${-/dev/stdin}) || "$#" -eq "2") ]]
+then
+    dims ${2:-/dev/stdin}
+elif [ "$1" = "add" ]
+then
+    # echo "Add"
+    add $2 $3
+    exit 0
+elif [ "$1" = "mean" ]
+then
+    echo "Mean"
+    # mean ${2:-/dev/stdin}
+elif [ "$1" = "multiply" ]
+then
+    # echo "Mult"
+    mult $2 $3
+    exit 0
+elif [ "$1" = "transpose" ]
+then
+    echo "Transpose"
+else
+    echo "That is not a valid command."
 fi
+
