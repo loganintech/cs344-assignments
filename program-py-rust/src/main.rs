bb052d4f18b85aa408bbd7fa80355e6c3f39cdbd
Logan Saso <logan.saso2016@gmail.com>
2018-10-11 15:21:11 -0700

    Some small changes

--- program-py-rust/src/main.rs
+++ program-py-rust/src/main.rs
@@ -1,16 +1,29 @@
 extern crate rand;
 
 use std::fs::File;
+use std::io::Write;
 
 use rand::prelude::*;
+use rand::distributions::Alphanumeric;
 
 fn main() {
     let mut rng = thread_rng();
 
-    let fileone = File::open("fileone.txt").unwrap();
-    let fieltwo = File::open("filetwo.txt").unwrap();
-    let filethree = File::open("filethree.txt").unwrap();
+    let fileone = File::create("fileone.txt").unwrap();
+    let filetwo = File::create("filetwo.txt").unwrap();
+    let filethree = File::create("filethree.txt").unwrap();
 
+    let charsone = rng.sample_iter(&Alphanumeric)
+                      .filter(|achar: &char| achar.is_ascii_alphabetic())
+                      .take(10)
+                      .collect::<String>();
 
+    fileone.write(charsone);
+
+    let first: u64 = rng.next_u32() as u64;
+    let second: u64 = rng.next_u32() as u64;
+    let product: u64 = first * second;
+
+    println!("{}\n{}\n{}", first, second, product);
 
 }
