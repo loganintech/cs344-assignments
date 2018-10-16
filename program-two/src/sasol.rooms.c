52216fd735012932215c4afe583ed8b38dcff9bf
Logan Saso <sasol@oregonstate.edu>
2018-10-16 02:43:55 -0700

    Added mostly working generation code. Valgrind none in heap

--- program-two/src/sasol.rooms.c
+++ program-two/src/sasol.rooms.c
@@ -3,6 +3,10 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
+
+#define false 0
+#define true 1
 
 enum RoomType
 {
@@ -13,39 +17,80 @@ enum RoomType
 
 struct Room
 {
-    char *name;
-    struct Room *connections;
+    int name_index;
+    struct Room **connections;
     enum RoomType type;
     int connection_count;
 };
 
 char *names[10] = {"dungeon", "castle", "shire", "poopdeck", "bedroom", "closet", "narnia", "whiterun", "skyrim", "vault"};
 
-//For safety, name and connections cannot be used after passed to this constructor
-struct Room new_room(char *name, struct Room *connections, enum RoomType type, int connection_count)
+/* For safety, name and connections cannot be used after passed to this constructor */
+struct Room *new_room(int name_index, enum RoomType type, int connection_count)
 {
-    struct Room room; //= malloc(sizeof(struct Room) * 1);
-    room.name = name,
-    room.connections = connections;
-    room.type = type;
-    room.connection_count = connection_count;
+    struct Room *room = malloc(sizeof(struct Room) * 1);
+    room->name_index = name_index,
+    room->connections = malloc(sizeof(struct Room *) * 6);
+    room->type = type;
+    room->connection_count = connection_count;
     return room;
 }
 
-struct Room *generate_7_rooms()
+int name_picked(int new_index, int *picked_names, int total_picked)
 {
-    struct Room *rooms = malloc(sizeof(struct Room) * 7);
-
-    for (int i = 0; i < 7; i++)
+    int i;
+    for (i = 0; i < total_picked; i++)
     {
+        if (picked_names[i] == new_index)
+        {
+            return false;
+        }
+    }
 
-        rooms[i] = new_room(names[i], NULL, START_ROOM, 0);
+    picked_names[total_picked] = new_index;
+    return true;
+}
+
+struct Room **generate_7_rooms()
+{
+    struct Room **rooms = malloc(sizeof(struct Room *) * 7);
+
+    int picked_names[7];
+    int i;
+    for (i = 0; i < 7; i++)
+    {
+        picked_names[i] = -1;
+    }
+
+    for (i = 0; i < 7; i++)
+    {
+        int new_name_index = rand() % 10;
+        while (name_picked(new_name_index, picked_names, i) == false)
+        {
+            new_name_index = rand() % 10;
+        }
+
+        enum RoomType type;
+        if (i == 0)
+        {
+            type = START_ROOM;
+        }
+        else if (i == 6)
+        {
+            type = END_ROOM;
+        }
+        else
+        {
+            type = MID_ROOM;
+        }
+
+        rooms[i] = new_room(new_name_index, type, 0);
     }
 
     return rooms;
 }
 
-//This is slow but frankly this program doesn't need to be that fast.
+/* This is slow but frankly this program doesn't need to be that fast. */
 char *concat_string(char *one, char *two)
 {
 
@@ -58,41 +103,45 @@ char *concat_string(char *one, char *two)
     return result;
 }
 
-char *write_to_file(char *room_string, char* room_name) {
-    // int pid = getpid();
-    // char snum[10];
-    // char *connection_string = sprintf(snum, "%d", pid);
+char *write_to_file(char *room_string, char *room_name)
+{
 
+    /*     int pid = getpid();
+    char snum[10];
+     char *connection_string = sprintf(snum, "%d", pid); */
 
-    FILE *file = fopen(concat_string(room_name, "_room"), "w");
+    char *filename = concat_string(room_name, "_room");
+    FILE *file = fopen(filename, "w");
     int results = fputs(room_string, file);
 
-    if (results == EOF) {
-        printf("Fuck");
+    if (results == EOF)
+    {
+        /*  printf("Failure"); */
     }
 
     fclose(file);
-
+    free(filename);
 }
 
-void print_room(struct Room room)
+void write_room_to_file(struct Room room)
 {
 
-    // Example setting for testing this function
-    int connection_count = 3;
-
-    char *file_format = concat_string("ROOM NAME: ", room.name);
+    /*  Example setting for testing this function */
+    char *file_format = malloc(sizeof(char) * 1024);
+    file_format = concat_string(file_format, "ROOM NAME: ");
+    file_format = concat_string(file_format, names[room.name_index]);
 
     file_format = concat_string(file_format, "\n");
 
-    for (int i = 0; i < connection_count && i < 10; i++)
+    int i;
+    for (i = 0; i < room.connection_count; i++)
     {
         char snum[2];
-        char *connection_string = (char*) sprintf(snum, "%d", i + 1);
+        sprintf(snum, "%d", i + 1);
         file_format = concat_string(file_format, "CONNECTION ");
         file_format = concat_string(file_format, snum);
         file_format = concat_string(file_format, ": ");
-        file_format = concat_string(file_format, room.name);
+        file_format = concat_string(file_format, names[room.connections[i]->name_index]);
         file_format = concat_string(file_format, "\n");
     }
 
@@ -115,23 +164,129 @@ void print_room(struct Room room)
 
     file_format = concat_string(file_format, "\n");
 
-    printf("%s\n", file_format);
+    /*  printf("%s\n", file_format); */
 
-    char room_name[10];
-    write_to_file(file_format, room.name);
+    write_to_file(file_format, names[room.name_index]);
 
     free(file_format);
-    // printf("ROOM NAME: %s\nCONNECTION: %d\nROOM TYPE: %s", room.name, );
+}
+
+/* The following code for generating a graph of rooms is outlined in 2.2 Program Outlining/* https/* oregonstate.instructure.com/courses/1725991/pages/2-dot-2-program-outlining-in-program-2/* Courtesy of Professor Brewster @ Oregon State University */
+/* An array of rooms */
+
+/* Gets a random index of the rooms to return */
+struct Room *get_random_room(struct Room **rooms, int room_count)
+{
+    return rooms[rand() % room_count];
+}
+
+/* Adds each room to the end of the other room's connections */
+void connect_rooms(struct Room *room_one, struct Room *room_two)
+{
+    /*  printf("Connecting rooms: %s - %s\n", names[room_one->name_index], names[room_two->name_index]); */
+
+    room_one->connections[room_one->connection_count++] = room_two;
+
+    room_two->connections[room_two->connection_count++] = room_one;
+}
+
+/* Tries to pick valid rooms to connect */
+void add_connection(struct Room **rooms, int room_count)
+{
+
+    struct Room *room_one;
+    struct Room *room_two;
+
+    /*  printf("Selecting Room One\n"); */
+    do
+    {
+        room_one = get_random_room(rooms, room_count);
+
+    } while (can_add_connection(room_one) == false);
+
+    /*  printf("Selecting Room Two\n"); */
+    do
+    {
+        room_two = get_random_room(rooms, room_count);
+    } while (can_add_connection(room_two) == false || is_same_room(room_one, room_two) == true || connection_exists(room_one, room_two) == true);
+
+    connect_rooms(room_one, room_two);
+}
+
+void generate_graph(struct Room **rooms, int room_count)
+{
+    while (graph_is_full(rooms, room_count) == false)
+    {
+        add_connection(rooms, room_count);
+    }
+}
+
+/* Checks if room's connection count is less than 6 */
+int can_add_connection(struct Room *room)
+{
+    /*  printf("Checking Can Add\n"); */
+    return room->connection_count < 6;
+}
+/* 0 for false, 1 for true */
+int graph_is_full(struct Room **rooms, int room_count)
+{
+
+    int i;
+    for (i = 0; i < room_count; i++)
+    {
+        if (rooms[i]->connection_count < 3)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+/* Returns true if the rooms have the same name and number of connections */
+int is_same_room(struct Room *room_one, struct Room *room_two)
+{
+    /*  printf("Checking Is Same\n"); */
+    return room_one->name_index == room_two->name_index;
+}
+
+int connection_exists(struct Room *room_one, struct Room *room_two)
+{
+    /*  printf("Checking Connection Exists\n"); */
+    int i, j;
+    for (i = 0; i < room_one->connection_count; i++)
+    {
+        /*  printf("Checking if %d is room %d\n", room_one->connections[i]->name_index, room_two->name_index); */
+        if (is_same_room(room_one->connections[i], room_two) == true)
+        {
+            /*  printf("%d is %d\n", room_one->connections[i]->name_index, room_two->name_index); */
+            return true;
+        }
+    }
+    return false;
 }
 
 int main()
 {
-    // printf("Pid found: %d\n", pid);
+    srand(time(NULL));
+    /* printf("Pid found: %d\n", pid); */
 
-    struct Room *rooms = generate_7_rooms();
-    for (int i = 0; i < 7; i++)
+    struct Room **rooms = generate_7_rooms();
+
+    generate_graph(rooms, 7);
+
+    /*  printf("Finished Generation\n"); */
+
+    int i, j;
+    for (i = 0; i < 7; i++)
     {
-        print_room(rooms[i]);
+        write_room_to_file(*rooms[i]);
+    }
+
+    for (i = 0; i < 7; i++)
+    {
+        free(rooms[i]->connections);
+        free(rooms[i]);
     }
 
     free(rooms);
