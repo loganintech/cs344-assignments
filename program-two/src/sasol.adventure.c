ba35a9349513d379fde8a59347548a189266d50f
Logan Saso <sasol@oregonstate.edu>
2018-10-22 01:18:35 -0700

    Leak-free, multithreaded for time, loads correct directory.

--- program-two/src/sasol.adventure.c
+++ program-two/src/sasol.adventure.c
@@ -1,13 +1,13 @@
-#include <pthread.h>
-#include <signal.h>
+#include <time.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <stdlib.h>
 #include <dirent.h>
 #include <string.h>
-#include <time.h>
+#include <pthread.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 #define true 1
 #define false 0
@@ -15,8 +15,9 @@
 
 pthread_t thread_id;
 pthread_mutex_t mutex_lock;
+pthread_mutex_t second_mutex;
 
-#pragma region create_rooms
+#pragma region loading_files
 
 char *names[10] = {"dungeon", "castle", "shire", "poopdeck", "bedroom", "closet", "narnia", "whiterun", "skyrim", "vault"};
 struct Room
@@ -131,7 +132,7 @@ struct Room *read_files(char *path)
             char *dir_path = malloc(sizeof(dirent_struct->d_name) + 250);
             memset(dir_path, '\0', sizeof(dir_path));
             sprintf(dir_path, "%s/%s", cwd, dirent_struct->d_name);
-            if (dirent_struct->d_name[0] != '.')
+            if (dirent_struct->d_name[0] != '.' && strcmp(dirent_struct->d_name, "currentTime.txt") != 0)
             {
                 /* printf("Reading File: %s\n", dirent_struct->d_name); */
                 /* printf("About to parse file: %s\n", dirent_struct->d_name); */
@@ -190,25 +191,43 @@ struct Room prompt_and_move(struct Room *rooms, struct Room current_room)
 
     name_buffer[strlen(name_buffer) - 1] = '\0';
 
-    int new_room_index;
-    for (new_room_index = 0; new_room_index < 7; new_room_index++)
+    for (connection_index = 0; connection_index < current_room.connection_count; connection_index++)
     {
-        if (strcmp(name_buffer, names[rooms[new_room_index].name_index]) == 0)
+        if (strcmp(name_buffer, current_room.connections[connection_index]) == 0 || strcmp(name_buffer, "time") == 0)
         {
-            return rooms[new_room_index];
-        } else if (strcmp(name_buffer, "time") == 0) {
-            printf("Unlocking mutex.");
+            int new_room_index;
+            for (new_room_index = 0; new_room_index < 7; new_room_index++)
+            {
+                if (strcmp(name_buffer, names[rooms[new_room_index].name_index]) == 0)
+                {
+                    return rooms[new_room_index];
+                }
+                else if (strcmp(name_buffer, "time") == 0)
+                {
+                    /* printf("Unlocking mutex.\n"); fflush(stdout); */
 
-            pthread_mutex_unlock(&mutex_lock);
-            sleep(1);
-            pthread_mutex_lock(&mutex_lock);
+                    int unlock_result = pthread_mutex_unlock(&mutex_lock);
+                    if (unlock_result != 0)
+                    {
+                        printf("Unlocking failed.");
+                        fflush(stdout);
+                    }
+                    sleep(0);
+                    pthread_mutex_lock(&mutex_lock);
+                    char *time_string = malloc(sizeof(char) * 1024);
+                    FILE *time_file = fopen("./currentTime.txt", "r");
+                    memset(time_string, '\0', 1024);
+                    size_t len = 1024;
 
-            FILE *time_file = fopen("./currentTime.txt", "r");
-            char *time_string = malloc(sizeof(char) * 100);
+                    size_t line_length = getline(&time_string, &len, time_file);
+                    time_string[line_length - 1] = '\0';
 
-            size_t line_length = getline(&time_string, sizeof(char) * 100, time_file);
-
-            printf("\n%s\n", time_string);
+                    fclose(time_file);
+                    printf("\n  %s\n", time_string);
+                    free(time_string);
+                    return current_room;
+                }
+            }
         }
     }
 
@@ -264,50 +283,137 @@ void run_game(struct Room *rooms)
 
 void *write_time()
 {
-
     while (true)
     {
-        pthread_mutex_lock(&mutex_lock);
+        int lock_result = pthread_mutex_lock(&mutex_lock);
+        if (lock_result != 0)
+        {
+            printf("Error locking in second thread.\n");
+        }
 
         FILE *time_file = fopen("./currentTime.txt", "w");
 
         const struct tm *right_now;
         time_t rawtime;
         time(&rawtime);
+
         right_now = localtime(&rawtime);
 
-        /* 1:03pm, Tuesday, September 13, 2016 */
+        char time_string[200];
+        memset(time_string, '\0', 200);
+        char newline[1] = {'\n'};
 
-        char *time_string = malloc(sizeof(char) * 100);
-        size_t length = strftime(time_string, sizeof(time_string), "%I:%M%p, %A, %m %e, %Y", right_now);
-        fwrite(time_string, sizeof(char), sizeof(time_string), time_file);
+        size_t length = strftime(time_string, 200, "%I:%M%p, %A, %m %e, %Y", right_now);
+
+        fwrite(time_string, sizeof(char), length, time_file);
+        fwrite(&newline, sizeof(char), 1, time_file);
         fclose(time_file);
+
         pthread_mutex_unlock(&mutex_lock);
+
+        size_t mutext_trylock = pthread_mutex_trylock(&second_mutex);
+        if (mutext_trylock == 0)
+        {
+            break;
+        }
     }
 
     return NULL;
 }
 
+char *get_directory()
+{
+
+    DIR *directory;
+    struct dirent *dirent_struct;
+    directory = opendir("./");
+
+    char **filenames = malloc(sizeof(char *) * 200);
+    memset(filenames, '\0', 200);
+    int filename;
+    for (filename = 0; filename < 200; filename++)
+    {
+        filenames[filename] = malloc(sizeof(char) * 100);
+        memset(filenames[filename], '\0', 100);
+    }
+    filename = 0;
+
+    int mtime = 0;
+    int nameindex = 0;
+
+    if (directory != NULL)
+    {
+        while (dirent_struct = readdir(directory))
+        {
+            if (dirent_struct->d_name[0] == '.')
+                continue;
+
+            if (strlen(dirent_struct->d_name) <= 12 || dirent_struct->d_name[5] != '.' || dirent_struct->d_name[11] != '.')
+                continue;
+
+            struct stat fileStat;
+            if (stat(dirent_struct->d_name, &fileStat) < 0)
+            {
+                printf("Stat didn't work for file %s.", dirent_struct->d_name);
+                continue;
+            }
+
+            if (mtime < fileStat.st_mtime)
+            {
+                mtime = fileStat.st_mtime;
+                nameindex = filename;
+            }
+
+            strcpy(filenames[filename++], dirent_struct->d_name);
+        }
+
+        closedir(directory);
+    }
+
+    char *real_filename = malloc(sizeof(char) * 256);
+    strcpy(real_filename, filenames[nameindex]);
+
+    int string_index;
+    for(string_index = 0; string_index < 200; string_index++) {
+        free(filenames[string_index]);
+    }
+
+    free(filenames);
+
+    return real_filename;
+}
+
 int main(char *argv)
 {
-    struct Room *rooms = read_files("./sasol.rooms.19818");
+
+    char *directory_location = get_directory();
+
+    struct Room *rooms = read_files(directory_location);
+    free(directory_location);
     if (pthread_mutex_init(&mutex_lock, NULL) != 0)
     {
-        printf("\nMutex creation failed.\n");
+        printf("Mutex creation failed.\n");
         return 1;
     }
-    printf("Created mutex");
+    pthread_mutex_lock(&mutex_lock);
+
+    if (pthread_mutex_init(&second_mutex, NULL) != 0)
+    {
+        printf("Mutex creation failed.\n");
+        return 2;
+    }
+    pthread_mutex_lock(&second_mutex);
 
     int pthread_err = pthread_create(&thread_id, NULL, write_time, NULL);
-    if (pthread_err != 0) {
-        printf("Can't create thread: %s", strerror(pthread_err));
+    if (pthread_err != 0)
+    {
+        printf("Can't create thread: %s\n", strerror(pthread_err));
     }
 
-    pthread_mutex_lock(&mutex_lock);
     run_game(rooms);
     pthread_mutex_unlock(&mutex_lock);
-
-    pthread_kill(&thread_id, SIGKILL);
+    pthread_mutex_unlock(&second_mutex);
+    pthread_join(thread_id, NULL);
 
     int room_index, connection_index;
     for (room_index = 0; room_index < 7; room_index++)
@@ -324,4 +430,3 @@ int main(char *argv)
 
     return 0;
 }
-
