eedafd568ae1457920d93c0c87e893e78c6d5d21
Logan Saso <sasol@oregonstate.edu>
2018-11-07 17:18:18 -0800

    Added start of program 3

---
+++ program-three/src/smallsh.c
@@ -0,0 +1,18 @@
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
+#include <pthread.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+int main(int argc, char* argv) {
+
+
+
+
+    return 0;
+}
